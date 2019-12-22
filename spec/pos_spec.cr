20fb27d231f0beaf3b9d6397b32941ea7f9efca3
Cristian Șerb <cserb@pm.me>
2019-12-22 13:41:31 +0100

    Removing validators

--- spec/pos_spec.cr
+++ spec/pos_spec.cr
@@ -1,16 +1,29 @@
 require "./spec_helper"
-require "../src/pos"
 
-describe PoS do
+describe Cocol::Pos do
+
+  before_each do
+    Cocol::Pos::ValidatorPool.validators.clear
+
+    Cocol::Pos::ValidatorPool.add(id: "bob", timestamp: 2_i64)
+    Cocol::Pos::ValidatorPool.add(id: "alice", timestamp: 1_i64)
+    Cocol::Pos::ValidatorPool.add(id: "dave", timestamp: 4_i64)
+    Cocol::Pos::ValidatorPool.add(id: "carol", timestamp: 3_i64)
+  end
+
   it "maintains order" do
-    PoS::ValidatorPool.add(id: "bob", timestamp: 2_i64)
-    PoS::ValidatorPool.add(id: "alice", timestamp: 1_i64)
-    PoS::ValidatorPool.add(id: "dave", timestamp: 4_i64)
-    PoS::ValidatorPool.add(id: "carol", timestamp: 3_i64)
+    validator_ids = Cocol::Pos::ValidatorPool.validators.map { |t| t[0] }
+    expected_validator_ids = %w(alice bob carol dave)
 
-    node_ids = PoS::ValidatorPool.nodes.map { |t| t[0] }
-    expected_node_ids = %w(alice bob carol dave)
+    validator_ids.should eq(expected_validator_ids)
+  end
 
-    node_ids.should eq(expected_node_ids)
+  it "removes validator with given id" do
+    Cocol::Pos::ValidatorPool.remove(id: "bob")
+
+    validator_ids = Cocol::Pos::ValidatorPool.validators.map { |t| t[0] }
+    expected_validator_ids = %w(alice carol dave)
+
+    validator_ids.should eq(expected_validator_ids)
   end
 end
--- src/pos.cr
+++ src/pos.cr
@@ -1,24 +1,32 @@
 require "big"
 
-module PoS
-  extend self
-  alias BlockHash = String
-
-  module ValidatorPool
+module Cocol
+  module Pos
     extend self
+    alias BlockHash = String
 
-    def nodes
-      @@nodes ||= Array(Tuple(String, Int64)).new
+    module ValidatorPool
+      extend self
+
+      def validators
+        @@validators ||= Array(Tuple(String, Int64)).new
+      end
+
+      def add(id : String, timestamp : Int64)
+        self.validators << {id, timestamp}
+        self.validators.sort! { |t1, t2| t1[1] <=> t2[1] }
+      end
+
+      def remove(id : String)
+        self.validators.reject! { |n| n[0] == id }
+      end
     end
 
-    def add(id : String, timestamp : Int64)
-      self.nodes << {id, timestamp}
-      self.nodes.sort! { |t1, t2| t1[1] <=> t2[1] }
-    end
-  end
+    def naive_random_selection(seed : BlockHash, validator_id : String) : Bool
+      bi = seed.to_big_i(16)
+      leader = ValidatorPool.validators[(bi % (ValidatorPool.validators.size + 1)) - 2]
 
-  def naive_random_selection(seed : BlockHash, node_id : String) : Bool
-    bi = seed.to_big_i(16)
-    ValidatorPool.nodes[bi % (ValidatorPool.nodes.size + 1)] == node_id
+      leader[0] == validator_id
+    end
   end
 end
